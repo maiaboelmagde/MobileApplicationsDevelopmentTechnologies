26a66fe900ae427202a8f8618873bdb267fb98d7
maiaboelmagd <maishalan00000@gmail.com>
2025-05-23 15:03:02 +0300

    1st task solution

---
+++ 02-DartFundamentals/Day1/task1/bin/task1.dart
@@ -0,0 +1,31 @@
+import 'package:task1/task1.dart' as function;
+import 'dart:io';
+
+void main(List<String> arguments) {
+
+  int num = -1;
+  bool exit = false;
+
+  while(!exit){
+    stdout.write('Enter a number (or "q" to exit):  ');
+    String? input = stdin.readLineSync();
+
+    if(input == 'q'){
+      exit = true;
+      continue;
+    }
+
+    
+    if(input != null){
+      int? num = int.tryParse(input);
+      if(num == null){
+        stdout.writeln('Error: Please enter a valid integer');
+      }else if(num < 0){
+        stdout.writeln('Error: Please enter a non-negative number.');
+      }else{
+        stdout.writeln('Factorial of $num is ${function.Factorial(num)}');
+      }
+
+    }
+  }
+}
---
+++ 02-DartFundamentals/Day1/task1/lib/task1.dart
@@ -0,0 +1,6 @@
+int Factorial(int num) {
+  if(num == 0)
+    return 1;
+
+  return num * Factorial(num-1); 
+}
